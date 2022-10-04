07fcdfbfe4e256b6b32a433ba7b5e0bd4af0e31f
resinner <revengingsinner@gmail.com>
2022-10-05 01:05:42 +0300

    task1 done

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const categories = document.querySelector("ul#categories");
+const categoriesItems = Array.from(categories.children).filter((category) =>
+  category.classList.contains("item")
+);
+
+console.log("Number of categories:", categoriesItems.length);
+
+const categoryElementsCount = categoriesItems.reduce((acc, el) => {
+  let title;
+  Array.from(el.children).forEach((child) => {
+    if (child.localName === "h2") {
+      title = child.innerText;
+    } else if (child.localName === "ul") {
+      acc[title] = child.children.length;
+    }
+  });
+  return acc;
+}, {});
+
+for (let key in categoryElementsCount) {
+  console.log("Category:", key);
+  console.log("Elements:", categoryElementsCount[key]);
+}
