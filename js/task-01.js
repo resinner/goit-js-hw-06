85fe745688e38b71dd215a401d3078b50627f63a
resinner <revengingsinner@gmail.com>
2022-10-04 19:13:36 +0300

    uploud to pc
    add src file

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const counter = (parent) => {
+  return parent.childElementCount;
+};
+const message = (elements) => {
+  elements.forEach((element) => {
+    const firstElem = element.firstElementChild;
+    const secondElem = firstElem.nextElementSibling;
+    console.log(`Category: ${firstElem.textContent}`);
+    console.log(`Elements: ${counter(secondElem)}`);
+  });
+};
+const categories = document.querySelector("#categories");
+const subCategories = categories.querySelectorAll(".item");
+console.log(`Number of categories: ${counter(categories)}`);
+message(subCategories);
