e8f77450097299903ca2d8752c2a56e672960fb5
resinner <revengingsinner@gmail.com>
2022-10-06 19:00:03 +0300

    task2 complited

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsContainer = document.querySelector("ul#ingredients");
+
+const template = ingredients.reduce(
+  (acc, item) => (acc += `<li class="item">${item}</li>`),
+  ""
+);
+
+ingredientsContainer.innerHTML = template;
+
+// other method
+
+// const ingredientsContainer = document.querySelector("ul#ingredients");
+// let containerItemsFragment = document.createDocumentFragment();
+
+// for (let ingridient of ingredients) {
+//   const li = document.createElement("li");
+//   li.innerText = ingridient;
+//   li.classList.add("item");
+//   containerItemsFragment.appendChild(li);
+// }
+
+// ingredientsContainer.appendChild(containerItemsFragment);
