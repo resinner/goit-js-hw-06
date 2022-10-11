14fa031f1e1bcbeb2082d5f02818b2b619bd28f7
resinner <revengingsinner@gmail.com>
2022-10-11 13:46:27 +0300

    Task8 complite

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit (event){
+    event.preventDefault();
+
+const formElements = event.currentTarget.elements;
+const email = formElements.email.value;
+const password = formElements.password.value;
+    
+    if (email === '' && password === '') {
+        alert('всі поля повинні бути заповнені')
+    } else {
+            const formData = {
+              email,
+              password,
+            };
+        
+        console.log(formData);
+        form.reset();
+    }
+}
