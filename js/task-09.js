d207fe78820fac54d92209c2966cd3196832991c
resinner <revengingsinner@gmail.com>
2022-10-11 15:35:02 +0300

    Task9 complite

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorRef = document.querySelector('button.change-color');
+const bodyRef = document.querySelector('body');
+const bgdColorRef = document.querySelector('span.color');
+
+btnChangeColorRef.addEventListener("click", onClickChange);
+
+function onClickChange(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  bgdColorRef.textContent = getRandomHexColor();
+}
+
