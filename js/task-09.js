414befab96edbad68a48e8f711b353c5fefc5b64
resinner <revengingsinner@gmail.com>
2022-10-12 14:56:29 +0300

    fix Task9

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const btnChangeColorRef = document.querySelector('button.change-color');
-const bodyRef = document.querySelector('body');
-const bgdColorRef = document.querySelector('span.color');
+const btnChangeColorRef = document.querySelector("button.change-color");
+const bodyRef = document.querySelector("body");
+const bgdColorRef = document.querySelector("span.color");
 
 btnChangeColorRef.addEventListener("click", onClickChange);
 
 function onClickChange(event) {
-  bodyRef.style.backgroundColor = getRandomHexColor();
-  bgdColorRef.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = randomColor;
+  bgdColorRef.textContent = randomColor;
 }
-
